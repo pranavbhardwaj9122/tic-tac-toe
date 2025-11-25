842edcec29d9954bab67bb75605445345573ecab
pranavbhardwaj9122 <pranavbhardwaj9122professional@gmail.com>
2025-11-25 23:57:47 +0530

    Fixed Winner Announcement

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const enableBoxes = () => {
 };
 
 const showWinner = (winner) => {
-    msg.innerText = 'Congratulations, Winner is ${winner}';
+    msg.innerText = `Congratulations, Winner is ${winner}`;
     msgContainer.classList.remove("hide");
     disableBoxes();
 };
